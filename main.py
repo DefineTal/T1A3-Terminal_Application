7dd0c6eabfa2a88a5e09e9283fd84b9611689d7b
DefineTal <taliesinstudy@gmail.com>
2023-12-18 19:09:08 +1100

    function cleanup v1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from blackjack_functions import showcards
 import random
 
 chips = 100
@@ -25,6 +24,31 @@ cards = {
     "Three" : 3,
     "Two" : 2
     }
+
+def showcards():
+    print(f"Your cards are: {player_cards}")
+    print(f"Your total value is: {player_value}\n")
+    
+    print(f"Dealers cards are: {dealer_cards}")
+    print(f"Dealer total value is: {dealer_value}\n")
+
+def resetround():
+    global player_cards, dealer_cards, player_value, dealer_value, roundstart
+    player_cards = []
+    dealer_cards = []
+    player_value = 0
+    dealer_value = 0
+    roundstart = True
+
+def dealcards(personcards, personvalue, x):
+    for i in range(0,x):
+        random_key, random_value = random.choice(list(cards.items()))
+        personcards.append(random_key)
+        personvalue += random_value
+    return personvalue
+
+
+
 prompt = "Please enter your name: "
 rules = "Your aim is to be closer to the number 21 with the dealt cards then the dealer. As the player you have the option to 'hit', to gain an extra card, 'stand' to keep ur value as is and end the round, 'surrender', to gain back half of the betted chips and lose the round. The value of the cards are: "
 
@@ -43,30 +67,19 @@ while player_input != "exit":
         chips -= int(chips_bet)
         print(f"You now have {chips} chips")
         roundstart = False
-
-        for i in range(0,2):
-            random_key, random_value = random.choice(list(cards.items()))
-            player_cards.append(random_key)
-            player_value += random_value
-
-        for i in range(0,2):
-            random_key, random_value = random.choice(list(cards.items()))
-            dealer_cards.append(random_key)
-            dealer_value += random_value
+        player_value = dealcards(player_cards, player_value, 2)
+        dealer_value = dealcards(dealer_cards, dealer_value, 2)
     
-    showcards(player_cards, player_value, dealer_cards, dealer_value)
+    showcards()
 
     player_input = input("Would you like to hit or stand? ")
 
     if player_input.lower() == "hit":
 
-        for i in range(0,1):
-            random_key, random_value = random.choice(list(cards.items()))
-            player_cards.append(random_key)
-            player_value += random_value
+        player_value = dealcards(player_cards, player_value, 1)
 
         if player_value > 21:
-            showcards(player_cards, player_value, dealer_cards, dealer_value)
+            showcards()
             print("You lose this round! Do you wanna play again? y/n?")
             player_input = input()
             try:
@@ -78,44 +91,28 @@ while player_input != "exit":
                         print("You dont have the chips to play again!")
                         break
                     else:
-                        player_cards = []
-                        dealer_cards = []
-                        player_value = 0
-                        dealer_value = 0
-                        roundstart = True
+                        resetround()        
             except:
                 print("Thats not a yes or no input! Try again")
 
         elif player_value <= 21:
-            showcards(player_cards, player_value, dealer_cards, dealer_value)
+            showcards()
             if dealer_value < 16:
-                for i in range(0,1):
-                    random_key, random_value = random.choice(list(cards.items()))
-                    dealer_cards.append(random_key)
-                    dealer_value += random_value
+                dealer_value = dealcards(dealer_cards, dealer_value, 1)
                 if dealer_value > 21:
-                    chips += chips_bet*2
+                    chips += int(chips_bet*2)
                     print(f"You win!\n You now have {chips} chips!")
-                    player_cards = []
-                    dealer_cards = []
-                    player_value = 0
-                    dealer_value = 0
-                    roundstart = True
+                    resetround()
+            
     elif player_input == "stand":
             while dealer_value < 16 or dealer_value <= player_value:                
-                for i in range(0,1):
-                    random_key, random_value = random.choice(list(cards.items()))
-                    dealer_cards.append(random_key)
-                    dealer_value += random_value
-                    showcards(player_cards, player_value, dealer_cards, dealer_value)
+                    dealer_value = dealcards(dealer_cards, dealer_value, 1)
+                    showcards()
+                
             if dealer_value > 21:                    
-                    chips += chips_bet*2
-                    print(f"You win!\n You now have {chips} chips!")
-                    player_cards = []
-                    dealer_cards = []
-                    player_value = 0
-                    dealer_value = 0
-                    roundstart = True
+                    chips += int(chips_bet)*2
+                    print(f"You win!\nYou now have {chips} chips!")
+                    resetround()
             else:
                 player_input = input()
                 try:
@@ -127,11 +124,7 @@ while player_input != "exit":
                             print("You dont have the chips to play again!")
                             break
                         else:
-                            player_cards = []
-                            dealer_cards = []
-                            player_value = 0
-                            dealer_value = 0
-                            roundstart = True
+                            resetround
                 except:
                     print("Thats not a yes or no input! Try again")
 
