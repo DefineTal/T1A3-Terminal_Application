5a327022aef398689c86a0d4757d772ac47677a2
DefineTal <taliesinstudy@gmail.com>
2023-12-20 03:34:46 +1100

    Make Game Pretty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+from rich import print
+from emoji import emojize
+
 
 # Initialising Variables
 chips = 100
@@ -33,11 +36,11 @@ class NegativeBet(Exception):
     pass
 # Function to show dealer and player value and cards
 def show_cards():
-    print(f"Your cards are: {player_cards}")
-    print(f"Your total value is: {player_value}\n")
+    print(f"[bold orange1]Your cards are: {player_cards}")
+    print(f"[bold orange1]Your total value is: {player_value}\n")
     
-    print(f"Dealers cards are: {dealer_cards}")
-    print(f"Dealer total value is: {dealer_value}\n")
+    print(f"[bold magenta]Dealers cards are: {dealer_cards}")
+    print(f"[bold magenta]Dealer total value is: {dealer_value}\n")
 # Function to reset variables after a hand is finished
 def reset_round():
     global player_cards, dealer_cards, player_value, dealer_value, roundstart
@@ -79,7 +82,7 @@ def player_win():
     print(f"You Win!\nYou now have {chips} chips!")  
 # Function for when player loses
 def player_lose():
-    print("You lose this round! Do you wanna play again? y/n?")
+    print("You lose this round!:crying_face: Do you wanna play again? [green1]y[/green1]/[red3]n[/red3]?")
     while True:
         player_input = input()
         try:
@@ -101,13 +104,13 @@ def player_lose():
             print("Thats not a yes or no input! Try again")
 # Function when called exits the game gracefully
 def exit_game():
-    print("\nThanks for playing!")
-    print("PROGRAM WILL NOW CLOSE")
+    print("\nThanks for playing!", ":thumbs_up:")
+    print("[bold]PROGRAM WILL NOW CLOSE")
     exit()
 
 # Text intro
 prompt = "Please enter your name: "
-rules = "Your aim is to be closer to the number 21 with the dealt cards then the dealer. As the player you have the option to 'hit', to gain an extra card, 'stand' to keep ur value as is and end the round, 'surrender', to gain back half of the betted chips and lose the round. The value of the cards are: "
+rules = "Your aim is to be closer to the number 21 but not over it with the dealt cards than the dealer. As the player you have the option to [bold]'hit'[/bold], to gain an extra card, [bold]'stand'[/bold] to keep ur value as is and end the round, [bold]'surrender'[/bold], to gain back half of the betted chips and lose the round. The value of the cards are: "
 
 print(prompt)
 # Determines player name
